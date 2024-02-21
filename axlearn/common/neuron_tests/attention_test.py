569cee928c3a12aed291ee163f6c9cadc1722ec9
Patrick Toulme <ptoulme@amazon.com>
2024-02-21 12:07:35 -0800

    Full Llama 7B layer fwd bwd

--- axlearn/common/neuron_tests/attention_test.py
+++ axlearn/common/neuron_tests/attention_test.py
@@ -3,6 +3,7 @@ import contextlib
 # pylint: disable=too-many-lines,duplicate-code,no-self-use
 
 import jax
+import pytest
 import numpy as np
 from jax import numpy as jnp
 from jax.sharding import PartitionSpec
@@ -48,10 +49,11 @@ def backup(self):
         utils.shapes(''),
     )
 class TransformerTest(NeuronTestCase):
-    """Tests ParallelTransformerLayer."""
+    """Tests TransformerLayer."""
 
-    def test_with_golden_value(self):
-        """A test of TransformerLayer by comparing results to a golden value."""
+    @pytest.mark.skip
+    def test_forward(self):
+        """A test of TransformerLayer forward."""
         mesh = jax.sharding.Mesh(np.array(jax.devices()).reshape(4, 8)[:, None, None, None, :],
                                  axis_names=("data", "seq", "expert", "fsdp", "model"),)
         with mesh:
@@ -146,6 +148,114 @@ class TransformerTest(NeuronTestCase):
                                              self._trainer_state_partition_specs))
             layer_outputs = run(mask, input_tensor, layer_params)
             self.assertEqual(target.shape, layer_outputs.data.shape)
+
+
+    def test_backward(self):
+        """A test of TransformerLayer backward."""
+        mesh = jax.sharding.Mesh(np.array(jax.devices()).reshape(4, 8)[:, None, None, None, :],
+                                 axis_names=("data", "seq", "expert", "fsdp", "model"),)
+        with mesh:
+            model_dim = 4096
+            num_heads = 32
+            cfg = TransformerLayer.default_config().set(name="test", input_dim=model_dim)
+            cfg.dtype = jnp.bfloat16
+            cfg.feed_forward.set(hidden_dim=scaled_hidden_dim(4))
+            cfg.self_attention.attention.set(num_heads=num_heads)
+            cfg.self_attention.attention.input_linear = FusedQKVLinear.default_config()
+            cfg.self_attention.norm = RMSNorm.default_config()
+            cfg.feed_forward.norm = RMSNorm.default_config()
+            set_bias_recursively(cfg, bias=False)
+            set_double_shard_weights_config(
+                cfg,
+                batch_axis_names='data',
+                fsdp_axis_names='fsdp',
+                tp_axis_names='model',
+                seq_axis_names='model',
+            )
+            layer: TransformerLayer = cfg.instantiate(parent=None)
+            self._trainer_state_specs = collect_param_specs(layer)
+            def create_named_sharding(param_spec, mesh):
+                if isinstance(param_spec, ParameterSpec):
+                    return NamedSharding(
+                        mesh,
+                        PartitionSpec(*param_spec.mesh_axes) if param_spec.mesh_axes != (None,) else PartitionSpec(None)
+                    )
+                return param_spec
+
+            def custom_tree_map(func, pytree, mesh):
+                if isinstance(pytree, dict) or isinstance(pytree, VDict):
+                    new_dict = {}
+                    for k, v in pytree.items():
+                        if k == 'i_proj': # Weird case where i_proj is a Vdict not a Dict
+                            new_dict[k] = VDict({sub_k: custom_tree_map(func, sub_v, mesh) for sub_k, sub_v in v.items()})
+                        else:
+                            new_dict[k] = custom_tree_map(func, v, mesh)
+                    return type(pytree)(new_dict)
+                else:
+                    return func(pytree)
+
+            self._trainer_state_partition_specs = custom_tree_map(
+                lambda ps: create_named_sharding(ps, mesh),
+                self._trainer_state_specs,
+                mesh
+            )
+            def init_cpu():  # Initing on Neuron causes compiler failures.
+                layer_params = layer.initialize_parameters_recursively(prng_key=jax.random.PRNGKey(0))
+                return layer_params
+
+            def move_to_neuron(params):
+                weights = jax.device_put(params)
+                return weights
+            cpu_device = jax.devices('cpu')[0]
+            with jax.default_device(cpu_device):
+                layer_params = init_cpu()
+
+            move_to_neuron = jax.jit(
+                move_to_neuron,
+                in_shardings=(self._trainer_state_partition_specs,), # tuple is necessary here
+            )
+            layer_params = move_to_neuron(layer_params)
+            def print_dict_structure(d, indent=0):
+                for key, value in d.items():
+                    print(' ' * indent + f"{key}: {type(value)}")
+                    if isinstance(value, dict):
+                        print_dict_structure(value, indent + 4)
+
+            #print_dict_structure(layer_params)
+
+            jax.debug.visualize_array_sharding(layer_params['feed_forward']['linear1']['weight'])
+            batch_size, tgt_len = 4, 4096
+            rng = np.random.default_rng(seed=123)
+            target = rng.random([batch_size, tgt_len, model_dim], dtype=np.float32).astype(jnp.bfloat16)
+            target = jax.device_put(target, NamedSharding(mesh, PartitionSpec('data', 'model', None)))
+            def mask_creation():
+                mask = attention.make_causal_mask(tgt_len).astype(jnp.bfloat16)
+                mask = jnp.tile(mask[None, None, :, :], (batch_size, num_heads, 1, 1))
+                mask = jax.device_put(mask)
+                return mask
+            mask_creation = jax.jit(mask_creation, out_shardings=NamedSharding(mesh, PartitionSpec('data', 'model', None, None)))
+            mask = mask_creation()
+            input_tensor = jnp.asarray(target).astype(jnp.bfloat16)
+            input_tensor = jax.device_put(input_tensor, NamedSharding(mesh, PartitionSpec('data', 'model', None)))
+
+            def run(mask, input_tensor, output_target, weights):
+                layer_outputs, _ = F(
+                    layer,
+                    inputs=dict(data=input_tensor, self_attention_logit_biases=mask),
+                    state=weights,
+                    is_training=True,
+                    prng_key=jax.random.PRNGKey(0),
+                )
+
+                return jnp.mean((layer_outputs.data - output_target) ** 2)
+
+            run = jax.jit(jax.value_and_grad(run), in_shardings=(NamedSharding(mesh, PartitionSpec('data', 'model', None, None)),
+                                             NamedSharding(mesh, PartitionSpec('data', 'model', None)),
+                                             NamedSharding(mesh, PartitionSpec('data', 'model', None)),
+                                             self._trainer_state_partition_specs))
+            loss, grad = run(mask, input_tensor, target, layer_params)
+            print(loss)
+            print(grad)
 
 
 
